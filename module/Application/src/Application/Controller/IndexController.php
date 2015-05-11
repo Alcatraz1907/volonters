fcaed7dc966932f01cd5775271780c2f9bda4160
illusi0n1 <vadym.t@mail.ru>
2015-05-11 15:05:11 +0300

    Merge branch 'master' of https://github.com/Alcatraz1907/volonters
    Conflicts:
    	module/Application/src/Application/Form/CategoriesSubForm.php

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -16,72 +16,17 @@ class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
+        $formDonors = new LoginDonorsForm();
+        $formVolunteers = new LoginVolunteersForm();
+        $formSearch= new SearchDonationsForm();
         return new ViewModel(array(
             'data' => array("data"=>"test2"),
-
+            'formDonors' => $formDonors,
+            'formVolunteers' => $formVolunteers,
+            'formSearch' => $formSearch
         ));
 
     }
 
-    public function donorLoginAction()
-    {
-        $form = new DonorLoginForm();
-        $form->get('submit')->setValue('Login');
-        $messages = null;
 
-        $request = $this->getRequest();
-        if ($request->isPost()) {
-            //- $authFormFilters = new User(); // we use the Entity for the filters
-            // TODO fix the filters
-            //- $form->setInputFilter($authFormFilters->getInputFilter());
-            // Filters have been fixed
-            $form->setInputFilter(new DonorLoginFilter($this->getServiceLocator()));
-            $form->setData($request->getPost());
-            // echo "<h1>I am here1</h1>";
-            if ($form->isValid()) {
-                $data = $form->getData();
-                $authService = $this->getServiceLocator()->get('Zend\Authentication\AuthenticationService');
-
-                $adapter = $authService->getAdapter();
-                $adapter->setIdentityValue($data['username']);
-                $adapter->setCredentialValue($data['password']);
-                $authResult = $authService->authenticate();
-
-                if ($authResult->isValid()) {
-                    $identity = $authResult->getIdentity();
-                    $authService->getStorage()->write($identity);
-                    //$time = 1209600; // 14 days 1209600/3600 = 336 hours => 336/24 = 14 days
-					//if ($data['rememberme']) $authService->getStorage()->session->getManager()->rememberMe($time); // no way to get the session
-                    //if ($data['rememberme']) {
-                    //    $sessionManager = new \Zend\Session\SessionManager();
-                    //    $sessionManager->rememberMe($time);
-                    //}
-                    //- return $this->redirect()->toRoute('home');
-                }
-                foreach ($authResult->getMessages() as $message) {
-                    $messages .= "$message\n";
-                }
-                /*
-                        $identity = $authenticationResult->getIdentity();
-                        $authService->getStorage()->write($identity);
-                        $authenticationService = $this->serviceLocator()->get('Zend\Authentication\AuthenticationService');
-                        $loggedUser = $authenticationService->getIdentity();
-                */
-            }
-        }
-        return new ViewModel(array(
-            'error' => 'Your authentication credentials are not valid',
-            'form'	=> $form,
-            'messages' => $messages,
-        ));
-    }
-
-    public function authTestAction()
-    {
-        if ($donors = $this->identity()) { // controller plugin
-            // someone is logged !
-        } else {
-            // not logged in
-        }
-    }
 }
