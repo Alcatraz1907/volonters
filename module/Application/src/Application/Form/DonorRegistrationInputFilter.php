0eba64f2178fe0c96cc0d5e98587016349fa1d6d
illusi0n1 <vadym.t@mail.ru>
2015-05-13 18:37:20 +0300

    Merge branch 'master' of https://github.com/Alcatraz1907/volonters
    Conflicts:
    	module/Application/src/Application/Form/CategoriesSubForm.php

---
+++ module/Application/src/Application/Form/DonorRegistrationInputFilter.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace Application\Form;
+use Zend\InputFilter\InputFilter;
+
+class DonorRegistrationInputFilter extends InputFilter {
+    public function __construct($sm)
+    {
+        $this->add(array(
+            'name' => 'name',
+            'required' => true,
+            'filters' => array(
+                array('name' => 'StripTags'),
+                array('name' => 'StringTrim'),
+            ),
+            'validators' => array(
+                array(
+                    'name' => 'StringLength',
+                    'options' => array(
+                        'encoding' => 'UTF-8',
+                        'min' => 1,
+                        'max' => 30,
+                    ),
+                ),
+            ),
+        ));
+        $this->add(array(
+            'name' => 'surname',
+            'required' => true,
+            'filters' => array(
+                array('name' => 'StripTags'),
+                array('name' => 'StringTrim'),
+            ),
+            'validators' => array(
+                array(
+                    'name' => 'StringLength',
+                    'options' => array(
+                        'encoding' => 'UTF-8',
+                        'min' => 1,
+                        'max' => 30,
+                    ),
+                ),
+            ),
+        ));
+        $this->add(array(
+            'name' => 'secondname',
+            'required' => true,
+            'filters' => array(
+                array('name' => 'StripTags'),
+                array('name' => 'StringTrim'),
+            ),
+            'validators' => array(
+                array(
+                    'name' => 'StringLength',
+                    'options' => array(
+                        'encoding' => 'UTF-8',
+                        'min' => 1,
+                        'max' => 30,
+                    ),
+                ),
+            ),
+        ));
+        $this->add(array(
+            'name' => 'phone',
+            'required' => true,
+            'filters' => array(
+                array('name' => 'StripTags'),
+                array('name' => 'StringTrim'),
+            ),
+            'validators' => array(
+                array(
+                    'name' => 'StringLength',
+                    'options' => array(
+                        'encoding' => 'UTF-8',
+                        'min' => 1,
+                        'max' => 10,
+                    ),
+                ),
+            ),
+        ));
+        $this->add(array(
+            'name' => 'skype',
+            'required' => true,
+            'filters' => array(
+                array('name' => 'StripTags'),
+                array('name' => 'StringTrim'),
+            ),
+            'validators' => array(
+                array(
+                    'name' => 'StringLength',
+                    'options' => array(
+                        'encoding' => 'UTF-8',
+                        'min' => 1,
+                        'max' => 30,
+                    ),
+                ),
+            ),
+        ));
+        $this->add(array(
+            'name' => 'facebook',
+            'required' => true,
+            'filters' => array(
+                array('name' => 'StripTags'),
+                array('name' => 'StringTrim'),
+            ),
+            'validators' => array(
+                array(
+                    'name' => 'StringLength',
+                    'options' => array(
+                        'encoding' => 'UTF-8',
+                        'min' => 1,
+                        'max' => 30,
+                    ),
+                ),
+            ),
+        ));
+        $this->add(array(
+            'name' => 'vkontakte',
+            'required' => true,
+            'filters' => array(
+                array('name' => 'StripTags'),
+                array('name' => 'StringTrim'),
+            ),
+            'validators' => array(
+                array(
+                    'name' => 'StringLength',
+                    'options' => array(
+                        'encoding' => 'UTF-8',
+                        'min' => 1,
+                        'max' => 30,
+                    ),
+                ),
+            ),
+        ));
+    }
+}
